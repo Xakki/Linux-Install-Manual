438032ea8d46d8168c4d4b48648a996783e9fa6e
Xakki <xakki@ya.ru>
2016-12-15 08:08:59 +0300

    Update bashlibs.sh

--- src/bashlibs.sh
+++ src/bashlibs.sh
@@ -2,6 +2,22 @@
 
 myAskYN() 
 {
+    local AMSURE
+    if [ -n "$1" ] ; then
+       read -n 1 -p "$1 (y/[n]): " AMSURE
+    else
+       read -n 1 AMSURE
+    fi
+    echo "" 1>&2
+    if [ "$AMSURE" = "y" ] ; then
+       return 0
+    else
+       return 1
+    fi
+}
+
+myAskVal()
+{
     if [ -n "$1" ] ; then
         read -p "$1 (по умолчанию $2): " projectName
     else
