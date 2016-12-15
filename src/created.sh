8045fef705a97bdddfce405d9a6edfb371bb7e7d
Xakki <xakki@ya.ru>
2016-12-15 08:06:58 +0300

    Update created.sh

--- src/created.sh
+++ src/created.sh
@@ -2,29 +2,49 @@
 
 ./bashlibs.sh
 
+vmDir="/home/kvm/vm"
+if [ -d "$vmDir" ]; then
+else
+    mkdir "$vmDir"
+fi
+
+isoFile="/home/kvm/debian-8.6.0-amd64-netinst.iso"
+if [ -f "$isoFile" ]; then
+else
+    myAskYN "ISO образ не найден. Скачать?" | exit 0
+    wget http://cdimage.debian.org/debian-cd/8.6.0/amd64/iso-cd/debian-8.6.0-amd64-netinst.iso
+fi
+
+
 projectName="test"
-myAskYN "Введите название виртмашины (оно же название фаила)" "$projectName"
+myAskVal "Введите название виртмашины (оно же название фаила)" "$projectName"
 
 projectCpu="2"
-myAskYN "Кол-во доступных процессоров" "$projectCpu"
+myAskVal "Кол-во доступных процессоров" "$projectCpu"
 
 projectRam="2"
-myAskYN "Объем доступной памяти в Гб" "$projectRam"
+myAskVal "Объем доступной памяти в Гб" "$projectRam"
 let "projectRam *= 1024"
 
-projectHdd="2"
-myAskYN "Объем диска в Гб" "$projectHdd"
+projectHdd="10"
+myAskVal "Объем диска в Гб" "$projectHdd"
+
+projectHost="test.example.ru"
+myAskVal "Укажите домен" "$projectHost"
+
+projectLastIp="2"
+myAskVal "Укажите последнюю цифру IP (192.168.11.X)" "$projectLastIp"
 
 virt-install \
 --connect qemu:///system \
 --name ${projectName} \
 --ram ${projectRam} \
 --vcpus ${projectCpu} \
---file /home/kvm/${projectName}.img \
+--file /home/kvm/vm/${projectName}.img \
 --file-size=${projectHdd} \
--c /usr/src/debian-8.6.0-amd64-netinst.iso \
+-c ${isoFile} \
 --initrd-inject=./pressed.cfg
---extra-args="auto keyboard-configuration/xkb-keymap=en"
+--extra-args="auto keyboard-configuration/xkb-keymap=en ip=192.168.11.$projectLastIp::192.168.11.1:255.255.255.0:$projectHost:eth0:none"
 --os-type linux
 --os-variant debianJessie \
 -w bridge:br0 \
@@ -34,6 +54,9 @@ virt-install \
 --accelerate \
 --autostart \
 --noautoconsole;
+
+#ip=192.168.1.2::192.168.1.1:255.255.255.0:test.example.com:eth0:none
+#ip=[ip]::[gateway]:[netmask]:[hostname]:[interface]:[autoconf]
 
 #--vnclisten=127.0.0.1 \
 #--graphics vnc,password=pass,listen=0.0.0.0,port=5903;
