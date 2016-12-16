e81a454e9da874c7d6b220927702f80581fee22f
Xakki <xakki@ya.ru>
2016-12-16 18:13:44 +0300

    Update created.sh

--- src/created.sh
+++ src/created.sh
@@ -6,7 +6,6 @@ vmDir="$baseDir/vm"
 pressedFile="$baseDir/pressed.cfg"
 isoFile="$baseDir/debian-8.6.0-amd64-netinst.iso"
 
-
 cd $baseDir
 
 if [ -f "$baseDir/bashlibs.sh" ]; then
@@ -44,44 +43,49 @@ else
     wget -nv https://raw.githubusercontent.com/Xakki/kvm.scripts/master/src/pressed.cfg
 fi
 
+# http://fai-project.org/fai-guide/
+apt-get install fai-quickstart -y
+
 echo
 
 projectName="test"
-myAskVal "Введите название виртмашины (оно же название фаила)" "$projectName"
+myAskVal "Введите название виртмашины (оно же название фаила)" "projectName"
 
 projectCpu="2"
-myAskVal "Кол-во доступных процессоров" "$projectCpu"
+myAskVal "Кол-во доступных процессоров" "projectCpu"
 
 projectRam="2"
-myAskVal "Объем доступной памяти в Гб" "$projectRam"
+myAskVal "Объем доступной памяти в Гб" "projectRam"
 let "projectRam *= 1024"
 
 projectHdd="10"
-myAskVal "Объем диска в Гб" "$projectHdd"
+myAskVal "Объем диска в Гб" "projectHdd"
 
 projectHost="test.example.ru"
-myAskVal "Укажите домен" "$projectHost"
+myAskVal "Укажите домен" "projectHost"
 
 projectLastIp="2"
-myAskVal "Укажите последнюю цифру IP (192.168.11.X)" "$projectLastIp"
+myAskVal "Укажите последнюю цифру IP (192.168.11.X)" "projectLastIp"
 
 virt-install \
 --connect qemu:///system \
---name ${projectName} \
---ram ${projectRam} \
---vcpus ${projectCpu} \
---file ${vmDir}/${projectName}.img \
---file-size=${projectHdd} \
--c ${isoFile} \
---initrd-inject=${pressedFile} \
+--name="$projectName" \
+--ram="$projectRam" \
+--vcpus="$projectCpu" \
+--file="$vmDir/$projectName.img" \
+--file-size="$projectHdd" \
+--location="http://ftp.nl.debian.org/debian/dists/jessie/main/installer-amd64/" \
+--initrd-inject="$pressedFile" \
 --extra-args="auto keyboard-configuration/xkb-keymap=en ip=192.168.11.$projectLastIp::192.168.11.1:255.255.255.0:$projectHost:eth0:none" \
---os-type linux \
---os-variant debianJessie \
+--os-type=linux \
+--os-variant=debianwheezy \
 -w bridge:br0 \
 --vnc \
 --vncport=5902 \
 --hvm \
 --accelerate \
 --autostart \
---noautoconsole;
+--noautoconsole \
+--debug;
+
 
