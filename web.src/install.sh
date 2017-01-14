acd8cc6bd13d62ac07ef39b4e921b4e782d16d17
Xakki <xakki@ya.ru>
2017-01-14 07:23:43 +0300

    Update install.sh

--- web.src/install.sh
+++ web.src/install.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 # Установка необходимого ПО на веб сервере (в частности на самой виртуалке)
 set -o nounset
+set -o errexit
 
 cd /tmp
 codeName="$(lsb_release -c -s)"
 apt-get -qq install lsb-release
 
-if [ 1 ]; then
-rm nginx_signing.key
-wget http://nginx.org/keys/nginx_signing.key
-apt-key add nginx_signing.key
-listName="/etc/apt/sources.list.d/nginx.list"
-echo "" > "$listName"
-echo "deb http://nginx.org/packages/debian/ $codeName nginx" >> "$listName"
-echo "deb-src http://nginx.org/packages/debian/ $codeName nginx" >> "$listName"
-echo "Nginx ready for instal - OK"
+if [[ 1 ]]; then
+  rm nginx_signing.key
+  wget http://nginx.org/keys/nginx_signing.key
+  apt-key add nginx_signing.key
+  listName="/etc/apt/sources.list.d/nginx.list"
+  echo "" > "$listName"
+  echo "deb http://nginx.org/packages/debian/ $codeName nginx" >> "$listName"
+  echo "deb-src http://nginx.org/packages/debian/ $codeName nginx" >> "$listName"
+  echo "Nginx ready for instal - OK"
 fi
 
-if [ 1 ]; then
-rm dotdeb.gpg
-wget https://www.dotdeb.org/dotdeb.gpg
-apt-key add dotdeb.gpg
-listName="/etc/apt/sources.list.d/dotdeb.list"
-echo "" > "$listName"
-echo "deb http://packages.dotdeb.org $codeName all" >> "$listName"
-echo "deb-src http://packages.dotdeb.org $codeName all" >> "$listName"
-echo "Doteb (PHP7) ready for instal - OK"
+if [[ 1 ]]; then
+  rm dotdeb.gpg
+  wget https://www.dotdeb.org/dotdeb.gpg
+  apt-key add dotdeb.gpg
+  listName="/etc/apt/sources.list.d/dotdeb.list"
+  echo "" > "$listName"
+  echo "deb http://packages.dotdeb.org $codeName all" >> "$listName"
+  echo "deb-src http://packages.dotdeb.org $codeName all" >> "$listName"
+  echo "Doteb (PHP7) ready for instal - OK"
 fi
 
 exit 0
